4505dd9e94207d2fb60e7da1a3c4e244e6a74352
Alistair Jones <alistair.jones@gmail.com>
2012-08-21 16:47:40 +0100

    Render each scenario as an independent chart with independent y scales, but sharing a single x scale.

--- src/main/web/report.js
+++ src/main/web/report.js
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-d3.tsv("/perf-charting/data.tsv", function(data) {
+d3.tsv("data.tsv", function(data) {
 
     function measurement( d )
     {
@@ -32,30 +32,39 @@ d3.tsv("/perf-charting/data.tsv", function(data) {
     var scenarios = ["avgr", "avgw", "peakr", "peakw", "susr", "susw"];
 
     var measurements = data.map(function(build) {
-        return scenarios.map(function(operation) {
+        return scenarios.map(function(scenario) {
             return {
                 build: buildTime(build),
-                operation: operation,
-                measurement: parseFloat(build[operation])
+                scenario: scenario,
+                measurement: parseFloat(build[scenario])
             };
         });
     }).reduce(function(a, b) {
         return a.concat(b);
     });
 
-    var chartSize = { width: 1024, height: 768},
-        margins = { left: 100, right: 100, top: 100, betweenCharts: 100, bottom: 300 },
+    var scenarioMeasurements = {};
+    measurements.forEach(function(measurement) {
+        var list = scenarioMeasurements[measurement.scenario];
+        if (!list) {
+            list = (scenarioMeasurements[measurement.scenario] = []);
+        }
+        list.push(measurement);
+    });
+
+    var chartSize = { width: 1024, height: 400},
+        margins = { left: 100, right: 100, top: 100, betweenCharts: 300, bottom: 300 },
         boundingBox = {
             width: chartSize.width + margins.left + margins.right,
             height: scenarios.length * (chartSize.height + margins.betweenCharts)
                 + margins.top + margins.bottom - margins.betweenCharts
         };
 
-    var chart = d3.select("div.results-container").selectAll("svg.chart" )
+    var svg = d3.select("div.results-container").selectAll("svg.chart" )
         .data([true])
         .enter()
         .append("svg:svg")
-        .attr("class", "chart")
+        .attr("class", "svg")
         .attr("viewBox", [
             -margins.left,
             -margins.top,
@@ -66,18 +75,31 @@ d3.tsv("/perf-charting/data.tsv", function(data) {
         .attr("height", boundingBox.height);
 
     var x = d3.scale.ordinal().domain(data.map(buildTime)).rangePoints([0, chartSize.width]);
-    var y = d3.scale.linear().domain([0, d3.max(measurements.map(measurement) )] ).range( [chartSize.height, 0] );
+    var xAxis = d3.svg.axis().scale(x).orient("left");
 
-    var xAxis = d3.svg.axis().scale(x ).orient("left");
-    var yAxis = d3.svg.axis().scale(y).orient("left");
+    var scenarioSpecificYScales = {};
+    scenarios.forEach(function(scenario) {
+        var yScale = d3.scale.linear()
+            .domain([0, d3.max(scenarioMeasurements[scenario].map(measurement) )] )
+            .range( [chartSize.height, 0] );
+        var yAxis = d3.svg.axis().scale(yScale).orient("left");
+        scenarioSpecificYScales[scenario] = { y: yScale, yAxis: yAxis };
+    });
 
-    var points = chart.selectAll("circle.measurement")
-        .data(measurements)
+    var chart = svg.selectAll("g.chart")
+        .data(scenarios)
+        .enter().append("svg:g")
+        .attr("class", "chart")
+        .attr("transform", function(d, i) { return "translate(0," + i * (chartSize.height + margins.betweenCharts) + ")" });
+
+
+    chart.selectAll("circle.measurement")
+        .data(function(scenario) { return scenarioMeasurements[scenario]; })
         .enter()
         .append("svg:circle")
         .attr("class", "measurement")
         .attr("r", 2)
-        .attr("cy", function(d) { return y( measurement(d)); })
+        .attr("cy", function(d) { return scenarioSpecificYScales[d.scenario].y(measurement(d)); })
         .attr("cx", function(d) { return x( d.build ); });
 
     chart.append("svg:g")
@@ -85,12 +107,20 @@ d3.tsv("/perf-charting/data.tsv", function(data) {
         .attr("transform", "rotate(270,0,0) translate(" + -chartSize.height + ", 0)")
         .call(xAxis);
 
-    chart.append("svg:g")
+    chart.selectAll("g.y.axis")
+        .data(function(d) { return [d]; })
+        .enter().append("svg:g")
         .attr("class", "y axis")
-        .call(yAxis);
+        .call(function(selection) {
+            selection.forEach(function(d) {
+                var group = d3.select(d[0]);
+                var axis = scenarioSpecificYScales[group.data()].yAxis;
+                axis(group);
+            });
+        });
 
     chart.append("svg:text")
         .attr("class", "axis-label y")
-        .attr("transform", "translate(" + 2 * -margin.left / 3 + " " + chartSize.height / 2 + ") rotate(-90)")
-        .text("Operations/s")
+        .attr("transform", "translate(" + 2 * -margins.left / 3 + " " + chartSize.height / 2 + ") rotate(-90)")
+        .text("Operations/s");
 });
