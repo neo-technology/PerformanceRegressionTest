4f688d52431ebe7b223e8c84ccf22ff79ffb90b2
Alistair Jones <alistair.jones@gmail.com>
2012-08-21 14:33:14 +0100

    Better handling of dimensions in preparation for splitting out charts by scenario.

--- src/main/web/report.js
+++ src/main/web/report.js
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-d3.tsv("data.tsv", function(data) {
+d3.tsv("/perf-charting/data.tsv", function(data) {
 
     function measurement( d )
     {
@@ -29,8 +29,10 @@ d3.tsv("data.tsv", function(data) {
         return d["build"];
     }
 
+    var scenarios = ["avgr", "avgw", "peakr", "peakw", "susr", "susw"];
+
     var measurements = data.map(function(build) {
-        return ["avgr",	"avgw",	"peakr", "peakw", "susr", "susw"].map(function(operation) {
+        return scenarios.map(function(operation) {
             return {
                 build: buildTime(build),
                 operation: operation,
@@ -41,19 +43,30 @@ d3.tsv("data.tsv", function(data) {
         return a.concat(b);
     });
 
-    var chartWidth = 1024, chartHeight = 768, margin = 300;
+    var chartSize = { width: 1024, height: 768},
+        margins = { left: 100, right: 100, top: 100, betweenCharts: 100, bottom: 300 },
+        boundingBox = {
+            width: chartSize.width + margins.left + margins.right,
+            height: scenarios.length * (chartSize.height + margins.betweenCharts)
+                + margins.top + margins.bottom - margins.betweenCharts
+        };
 
     var chart = d3.select("div.results-container").selectAll("svg.chart" )
         .data([true])
         .enter()
         .append("svg:svg")
         .attr("class", "chart")
-        .attr("viewBox", [-margin,-margin,chartWidth + margin * 2,chartHeight + margin * 2].join(" "))
-        .attr("width", chartWidth + margin * 2)
-        .attr("height", chartHeight + margin * 2);
+        .attr("viewBox", [
+            -margins.left,
+            -margins.top,
+            boundingBox.width,
+            boundingBox.height
+        ].join(" "))
+        .attr("width", boundingBox.width)
+        .attr("height", boundingBox.height);
 
-    var x = d3.scale.ordinal().domain(data.map(buildTime)).rangePoints([0, chartWidth]);
-    var y = d3.scale.linear().domain([0, d3.max(measurements.map(measurement) )] ).range( [chartHeight, 0] );
+    var x = d3.scale.ordinal().domain(data.map(buildTime)).rangePoints([0, chartSize.width]);
+    var y = d3.scale.linear().domain([0, d3.max(measurements.map(measurement) )] ).range( [chartSize.height, 0] );
 
     var xAxis = d3.svg.axis().scale(x ).orient("left");
     var yAxis = d3.svg.axis().scale(y).orient("left");
@@ -69,7 +82,7 @@ d3.tsv("data.tsv", function(data) {
 
     chart.append("svg:g")
         .attr("class", "x axis")
-        .attr("transform", "rotate(270,0,0) translate(" + -chartHeight + ", 0)")
+        .attr("transform", "rotate(270,0,0) translate(" + -chartSize.height + ", 0)")
         .call(xAxis);
 
     chart.append("svg:g")
@@ -78,6 +91,6 @@ d3.tsv("data.tsv", function(data) {
 
     chart.append("svg:text")
         .attr("class", "axis-label y")
-        .attr("transform", "translate(" + 2 * -margin / 3 + " " + chartHeight / 2 + ") rotate(-90)")
+        .attr("transform", "translate(" + 2 * -margin.left / 3 + " " + chartSize.height / 2 + ") rotate(-90)")
         .text("Operations/s")
 });
